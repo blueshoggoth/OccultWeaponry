6238bd7878333e00b6756f170ceb5ecaae43bb19
Raffaele Orabona <80917080+TheOccultist@users.noreply.github.com>
2022-02-25 23:15:22 +0100

    Delete OccultWeaponary directory

--- OccultWeaponary/src/main/java/com/at89/occultweaponry/client/util/ClientUtils.java
+++
@@ -1,9 +0,0 @@
-package com.at89.occultweaponry.client.util;
-
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
-
-@OnlyIn(Dist.CLIENT)
-public class ClientUtils {
-
-}
